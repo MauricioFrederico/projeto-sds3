c4e079f24157da9b3b3a15e6968ebe5fd688a1f6
Mauricio Frederico <maufrederico@yahoo.com.br>
2021-09-06 13:29:03 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
@@ -7,7 +9,25 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá Mundo</h1>
+        <h1 className="text-primary py-3">DashBoard de Vendas</h1>
+
+        <div className="row px-3">
+          {/* col-sm-6 -> bootstrap separa na horizontal em 12 partes, então estou utilizando a metade */}
+          <div className=" col-sm-6">
+            <h5 className="text-center text-secundary">Taxa de Sucesso (%)</h5>
+            <BarChart />
+          </div>
+          <div className=" col-sm-6">
+            <h5 className="text-center text-secundary">Todas Vendas</h5>
+            <DonutChart />
+          </div>
+
+        </div>
+    
+        <div className="py-3">
+          <h2 className="text-primary">Todas Vendas</h2>
+        </div>
+
         <DataTable />
       </div>
       <Footer />

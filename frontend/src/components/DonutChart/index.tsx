c86be6450fa8472b6b84b989d9f88a0f48d5de8b
Mauricio Frederico <mauricio.frederico@sanasa.com.br>
2021-09-10 15:30:30 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -13,51 +13,52 @@ type ChartData = {
 const DonutChart = () => {
 
     //forma certa
-    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: []});
+    const [chartData, setChartData] = useState<ChartData>({ labels: [], series: [] });
 
     //forma certa
     useEffect(() => {
         axios.get(`${BASE_URL}/sales/amount-by-seller`)
-        .then( response => {
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-            setChartData ({ labels: myLabels, series: mySeries});
-           
-        });
-    },[]);
-   
-    //forma errada
-   // let chartData : ChartData = { labels: [], series: []};
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChartData({ labels: myLabels, series: mySeries });
+
+            });
+    }, []);
 
     //forma errada
-  //  axios.get(`${BASE_URL}/sales/amount-by-seller`)
-  //      .then( response => {
-  //          const data = response.data as SaleSum[];
-   //         const myLabels = data.map(x => x.sellerName);
+    // let chartData : ChartData = { labels: [], series: []};
+
+    //forma errada
+    //  axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    //      .then( response => {
+    //          const data = response.data as SaleSum[];
+    //         const myLabels = data.map(x => x.sellerName);
     //        const mySeries = data.map(x => x.sum);
     //        setChartData ({ labels: myLabels, series: mySeries});
     //        console.log(chartData);
     //    });
 
 
-    const mockData = {
+ /*   const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
         labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     }
-    
+*/
     const options = {
         legend: {
             show: true
         }
     }
-    
-    
-    
-    
+
+
+
+
     return (
-        <Chart  
-            options = {{ ...options, labels: chartData.labels}}
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
             series={chartData.series}
             type="donut"
             height="240"
